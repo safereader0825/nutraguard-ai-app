112c29486b2304bbaeb40d1900b12d88c54862f3
safereader0825 <nutraguard.ai@gmail.com>
2025-08-12 21:56:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
